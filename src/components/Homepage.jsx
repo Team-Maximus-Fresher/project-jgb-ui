275f7ac2f45c34e9d4462b772ea65004d49849e4
Team-Maximus-Fresher <nishchithkulkarni496@gmail.com>
2021-02-21 18:12:04 +0530

    Updated Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React , {useState} from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Container } from '@material-ui/core';
+
 
 function Homepage(props) {
+
+    const [count, setCount] = useState(0);
+
     return (
         <div>
-            <h2>Hello</h2>
+            <Container fixed>
+                <Grid item md={6} style={{border : "2px solid black"}}>
+                    <h2>Hello</h2>
+                </Grid>
+            </Container>
         </div>
     );
 }
